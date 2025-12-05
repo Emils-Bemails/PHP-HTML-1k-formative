cf9805b5568c7f1404d56e63c13498b42a0e03ed
you@example.com <you@example.com>
2025-12-05 15:29:57 +0200

    4 uzdevums izpildits

---
+++ contact-sent.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Veiksmīgi nosūtijāt datus!" . "<br>";
+    echo($_POST["fname"]) . "<br>";
+    echo($_POST["fepasts"]) . "<br>";
+    echo($_POST["fatsauksmes"]). "<br>";
+    echo "<a href=contact.html>Iet atpakal uz Contact.html</a>";
+
+
+    ?>
+
+    
+</body>
+</html>
